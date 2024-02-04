7985b4ef2b2987dbc6c772be390bc0944d86ace9
Ian Cedric Io <82087099+cedric130813@users.noreply.github.com>
2024-02-04 21:33:45 +0800

    Butterfly Plot (EduVizzers Feb 2024) Upload

---
+++ R Stats/Butterfly Plot (EduVizzers Feb 2024)/EduVizzersFeb.R
@@ -0,0 +1,70 @@
+# Load library
+library(data.table)
+library(tidyverse)
+library(hrbrthemes)
+library(patchwork)
+
+# Load the dataset
+EduData = read.csv("SPP_compared_to_SPS_Kindergarteners_20240131.csv")
+
+View(EduData)
+class(EduData)
+
+# Replace <10 string with integer 
+EduData[EduData$Count.of.Children.Served=="<10",3]=10
+EduData$Count.of.Children.Served = as.integer(EduData$Count.of.Children.Served)
+class(EduData$Count.of.Children.Served)
+
+# https://www.njtierney.com/post/2022/08/09/ggplot-pyramid/
+
+EduDataSPP = EduData %>%
+  filter(Program.Year=="SPP 2018 - 2019 4 Year-Olds") %>%
+  filter(Group %in% c("White","Vietnamese",
+                 "Two or More Races","Spanish",
+                 "Other Asian Languages","Other & Unknown Languages",
+                 "Native Hawaiian/ Other Pacific Islander","Male","Hispanic/ Latino",
+                 "Female","English","Chinese- All Dialects","Black/African-American",
+                 "Asian","American Indian/ Alaskan Native","African Languages"))
+
+EduDataSPS = EduData %>%
+  filter(Program.Year=="SPS 2018 - 2019") %>%
+  filter(Group %in% c("White","Vietnamese",
+                      "Two or More Races","Spanish",
+                      "Other Asian Languages","Other & Unknown Languages",
+                      "Native Hawaiian/ Other Pacific Islander","Male","Hispanic/ Latino",
+                      "Female","English","Chinese- All Dialects","Black/African-American",
+                      "Asian","American Indian/ Alaskan Native","African Languages"))
+
+p1 = ggplot(EduDataSPS,
+       aes(x = Count.of.Children.Served,
+           y = Group,
+           fill = Count.of.Children.Served)) + 
+  geom_col(show.legend=FALSE) + 
+  labs(subtitle ="SPS 2018 - 2019") + geom_text(aes(label = Count.of.Children.Served),colour="white")
+
+p1 = p1 + theme_modern_rc() + theme(axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(), 
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank())
+
+p1
+ 
+p2 = ggplot(EduDataSPP,
+       aes(x = -Count.of.Children.Served,
+           y = Group,
+           fill = Count.of.Children.Served)) + 
+  geom_col(show.legend=FALSE) + labs(subtitle="SPP 2018 - 2019") +
+  xlim(-4000, 0) + geom_text(aes(label = Count.of.Children.Served),colour="white")
+
+p2 = p2 + theme_modern_rc() + theme(axis.text.x = element_blank(), 
+                axis.ticks.x = element_blank(),
+                axis.title.x = element_blank()) 
+
+p2+p1 + plot_annotation(caption = 'EduVizzers (Feb 2024) made in R',
+                  theme=theme(plot.title=element_text(hjust=0.5)))
+
+
+
+
