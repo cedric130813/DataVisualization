a19d1a234ae3ff8023097960c38e5ea60130bb9b
Ian Cedric Io <82087099+cedric130813@users.noreply.github.com>
2024-02-14 15:48:19 +0800

    Updated Tableau EDA

--- Tableau/Supply Chain Datasets (2024)/Supply Chain Sustainability Dataset/sustainability.R
+++ Tableau/Supply Chain Datasets (2024)/Supply Chain Sustainability Dataset/sustainability.R
@@ -10,7 +10,14 @@ uomConvert = read.csv("uom_conversions.csv", stringsAsFactors = T)
 masterOrder = merge(uomConvert, orders, by = 'Item.Code')
 
 # split gps loc's location using comma separator
-gps_loc = gps_loc %>% 
+masterGpsLoc = gps_loc %>% 
   separate_wider_delim(Location, ", ", names = c("Country", "City"), too_many = "merge")
 
 # ... to be continued
+
+# total value of customer order
+masterOrder$TotalValue= masterOrder$Units * masterOrder$Euros
+
+write.csv(masterGpsLoc,file = "masterGps.csv")
+write.csv(masterOrder,file = "masterOrder.csv")
+write.csv(distances, file = "masterDistance.csv")
